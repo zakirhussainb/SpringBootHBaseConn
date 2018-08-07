c02a639aeceb0e94ace9aaab6912c82a18f449eb
zakirhussainb <zakirhussainb2693@gmail.com>
2018-08-07 20:30:55 +0530

    Updated main class

--- src/main/java/com/example/jpa/TestHBaseConn.java
+++ src/main/java/com/example/jpa/TestHBaseConn.java
@@ -1,11 +1,5 @@
 package com.example.jpa;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestHBaseConn {
 
     public static void main(String[] args) throws Exception {
-//        args[0] = "18.188.77.36";
-//        args[1] = "2181";
         SpringApplication.run(TestHBaseConn.class, args);
     }
 }
